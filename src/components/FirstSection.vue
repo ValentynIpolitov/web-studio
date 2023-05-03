f9d1b74d82a9529cc9c3ac5149896a19460862f5
Valentyn Ipolitov <sv.vartey@gmail.com>
2023-05-03 02:55:05 +0200

    first section ready

--- src/components/FirstSection.vue
+++ src/components/FirstSection.vue
@@ -1,13 +1,16 @@
 <template>
     <div class="w-full min-h-screen bg-[url('@/assets/main-bg.jpg')] bg-fixed bg-no-repeat bg-center bg-cover pt-16  flex flex-col	justify-center	align-start">
-        <div class="container m-auto px-6">
-            <div class="flex flex-col justify-center items-center md:items-start">
+        <div class="container m-auto px-6 flex flex-col gap-y-8 xl:flex-row">
+            <div class="flex flex-col justify-center items-center md:items-start xl:w-1/2">
                 <h1 class="text-white uppercase mb-12 text-center md:text-left">Web development studio</h1>
                 <p class="text-white mb-12 text-center md:text-left">
                     “The last ten years of IT have been about changing the way people work. 
                     <br>The next ten years of IT will be about transforming your business.” 
                 </p>
                 <Button class="btn-secondary">Our services</Button>
+            </div>
+            <div class="xl:w-1/2">
+                <img class="w-auto" src="@/assets/main-1.png" alt="">
             </div>
         </div>
     </div>
